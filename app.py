291901d3edb4de5697827a08e7e23a95e549c283
rotsoc <socrtjin@gmail.com>
2019-03-13 17:04:13 -0700

    Flask lecture

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
-from flask import *
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+notes = []
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if session.get("notes") is None:
+        session["notes"] = []
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
+        
+    return render_template("index.html", notes=session["notes"])
+
+@app.route("/hello", methods=["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template("hello.html", name=name)
